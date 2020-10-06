1b609ec3da3938d7b454425c11b7e658e610f84c
Austin Goetz <a.goetz09@gmail.com>
2020-10-06 15:26:51 -0600

    adds project file

---
+++ YikYakMASTER/YikYakMASTER/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  YikYakMASTER
+//
+//  Created by Austin Goetz on 10/6/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
